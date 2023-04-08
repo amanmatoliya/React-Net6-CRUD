505b226e824ade57a04e2fd2a721536f8b8a2c6e
AmanMatoliya <AmanMatoliya@HSHLT-1934>
2023-04-08 16:40:34 +0530

    Add project files.

---
+++ Sundues_Employee_CRUD/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace Sundues_Employee_CRUD.Models
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public string Designation { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+    }
+}
