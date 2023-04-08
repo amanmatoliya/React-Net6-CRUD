69accfaf6e27b558c268358a983ca35e368a9491
Aman Matoliya <aman.matoliya@hanu.com>
2023-04-08 23:58:52 +0530

    Update and Delete employee functionality added in employee controller

--- Sundues_Employee_CRUD/Controllers/EmployeeController.cs
+++ Sundues_Employee_CRUD/Controllers/EmployeeController.cs
@@ -1,13 +1,78 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sundues_Employee_CRUD.Data;
+using Sundues_Employee_CRUD.Models;
 
 namespace Sundues_Employee_CRUD.Controllers
 {
     [ApiController]
+    [Route("api/[controller]")]
     public class EmployeeController : Controller
     {
-        public IActionResult Index()
+        private readonly EmployeeDbContext _employeeDbContext;
+
+        public EmployeeController(EmployeeDbContext employeeDbContext)
         {
-            return View();
+            _employeeDbContext = employeeDbContext;
+        }
+
+        [HttpGet("GetAllEmployees")]
+        public async Task<IActionResult> GetAllEmployees()
+        {
+            var emp = await _employeeDbContext.Employees.ToListAsync();
+            return Ok(emp);
+        }
+
+        [HttpGet("GetEmployeeById")]
+        public async Task<IActionResult> GetEmployeeById(int id)
+        {
+            var emp = await _employeeDbContext.Employees.FirstOrDefaultAsync(e=>e.Id == id);
+            if (emp == null) { return NotFound(); }
+            return Ok(emp);
+        }
+        [HttpPost("Add_Employee")]
+        public async Task<IActionResult> CreateEmployee(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                _employeeDbContext.Employees.Add(employee);
+                await _employeeDbContext.SaveChangesAsync();
+                return Ok("Employee Added");
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        [HttpDelete("Delete_Employee/{id}")]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            var emp = await _employeeDbContext.Employees.FindAsync(id);
+
+            if (emp == null)
+            {
+                return NotFound();
+            }
+
+            _employeeDbContext.Employees.Remove(emp);
+            await _employeeDbContext.SaveChangesAsync();
+
+            return Ok("Record Deleted Successfully");
+        }
+
+        [HttpPut("Update_Employee/{id}")]
+        public async Task<IActionResult> UpdateEmployee(int id, Employee employee)
+        {
+            var emp = await _employeeDbContext.Employees.FindAsync(id);
+            if (emp != null) {
+                emp.First_Name = employee.First_Name;
+                emp.Last_Name= employee.Last_Name;
+                emp.Designation= employee.Designation;
+                emp.Email = employee.Email;
+                emp.Phone = employee.Phone;
+                await _employeeDbContext.SaveChangesAsync();
+                return Ok("Updated successfully");
+            }
+            return NotFound();
         }
     }
 }
